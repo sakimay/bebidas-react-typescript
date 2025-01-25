ae2f7b3a97b46dee64c3b4ae7102ce2cb4d60c86
Isaac <mayne.isaac@gmail.com>
2025-01-25 22:24:27 +0100

    add form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
-import { Link, NavLink } from "react-router-dom"
+import { useMemo } from "react"
+import { NavLink, useLocation } from "react-router-dom"
+
 export default function Header() {
+    const { pathname } = useLocation()
+    const isHome = useMemo(() => pathname === '/', [pathname])
+    console.log(isHome);
+
     return (
         <header className="bg-slate-800">
             <div className="mx-auto container px-5 py-16">
@@ -7,9 +13,57 @@ export default function Header() {
                     <img src="./logo.svg" alt="" className="w-32" />
                 </div>
                 <nav className="flex gap-4">
-                    <Link to="/" className="text-white uppercase font-bold">Inicio</Link>
-                    <Link to="/favoritos" className="text-white uppercase font-bold">Favoritos</Link>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive
+                                ? 'text-orange-500 uppercase font-bold'
+                                : 'text-white uppercase font-bold'
+                        }
+                    >Inicio</NavLink>
+                    <NavLink
+                        to="/favoritos"
+                        className={({ isActive }) =>
+                            isActive
+                                ? 'text-orange-500 uppercase font-bold'
+                                : 'text-white uppercase font-bold'
+                        }
+                    >Favoritos</NavLink>
                 </nav>
+
+                {isHome && (
+                    <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="ingredient"
+                                className="text-white block uppercase font-extrabold text-lg"
+                            >Nombre o Ingredientes</label>
+                            <input
+                                id="ingredient"
+                                type="ingredient"
+                                className="bg-white p-3 w-full rounded-lg focus:outline-none"
+                                placeholder="Vodka, tequila, café..."
+                            />
+                        </div>
+                        <div className="space-y-4">
+                            <label
+                                htmlFor="category"
+                                className="text-white block uppercase font-extrabold text-lg"
+                            >Categoría</label>
+                            <select
+                                name="category"
+                                id="category"
+                                className="bg-white p-3 w-full rounded-lg focus:outline-none"
+                            >
+                                <option value="">-- Seleccionar --</option>
+                            </select>
+                        </div>
+                        <input
+                            type="submit"
+                            value="Buscar Bebidas"
+                            className="bg-orange-800 p-3 w-full rounded-lg uppercase font-extrabold text-lg hover:cursor-pointer text-white" />
+                    </form>
+                )}
             </div>
         </header>
     )
