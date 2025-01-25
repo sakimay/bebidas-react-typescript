a3a657725082e7b52c65f76969f1b74c71074a7c
Isaac <mayne.isaac@gmail.com>
2025-01-25 22:04:14 +0100

    add layout and tailwind

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+
+export default function Header() {
+    return (
+        <header className="bg-slate-800">
+            <div className="mx-auto container px-5 py-16">
+                <div className="flex justify-between items-center">
+                    <img src="./logo.svg" alt="" className="w-32" />
+                </div>
+                <nav>
+
+
+                </nav>
+            </div>
+        </header>
+    )
+}
