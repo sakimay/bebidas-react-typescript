86e08146913d18dfc3925233c80a26eff3cb3518
Isaac <mayne.isaac@gmail.com>
2025-02-02 22:55:27 +0100

    add grid recipies

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -6,9 +6,22 @@ type DrinkCardProps = {
 
 export default function DrinkCard({ drink }: DrinkCardProps) {
     return (
-        <div>
-            <h2>{drink.strDrink}</h2>
-            <img src={drink.strDrinkThumb} alt={drink.strDrink} />
+        <div className="shadow-lg">
+            <div className="overflow-hidden">
+                <img
+                    src={drink.strDrinkThumb}
+                    alt={`Imagen de ` +drink.strDrink}
+                    className="hover:scale-125 transition-all hover:rotate-2"
+                />
+            </div>
+            <div className="p-5">
+                <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+                <button
+                    type="button"
+                    className="bg-orange-400 hover:bg-amber-500 mt-5 w-full p-3 font-bold text-white text-lg"
+                >Ver receta
+                </button>
+            </div>
         </div>
     )
 }
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -12,14 +12,14 @@ export default function IndexPage() {
       <h1 className="text-6xl font-extrabold">Inicio</h1>
 
       {hasDrinks ? (
-        <>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 my-10">
           {drinks.drinks.map((drink) => (
             <DrinkCard
               key={drink.idDrink}
               drink={drink} />
           ))}
 
-        </>
+        </div>
       ) : (
         <>
           <p className="my-10 text-center text-2xl">
