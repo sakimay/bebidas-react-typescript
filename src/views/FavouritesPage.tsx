333e2c1db2446fccdc5de6a30a97bf8b1d960728
Isaac <mayne.isaac@gmail.com>
2025-02-03 22:54:45 +0100

    add favourites page

--- src/views/FavouritesPage.tsx
+++ src/views/FavouritesPage.tsx
@@ -1,9 +1,16 @@
-
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
 
 export default function FavouritesPage() {
+    const favourites = useAppStore((state) => state.favourites)
     return (
         <>
-           <h1>Favoritos</h1>
+            <h1 className="text-6xl font-extrabold">Favoritos</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 my-10">
+                {favourites.map((recipie) => (
+                    <DrinkCard key={recipie.idDrink} drink={recipie} />
+                ))}
+            </div>
         </>
     )
 }
